f7fb2534e3a83b70cda7b7ca81e7025235ca860e
shiyanshirani <shiyan99s@gmail.com>
2021-09-19 23:35:12 +0530

    completed

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.home),
+    path("", views.home, name="home"),
+    path("update/<str:pk>/", views.update_task, name="update_task"),
+    path("delete/<str:pk>/", views.delete_task, name="delete_task"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework.decorators import api_view
-from .forms import TaskForm
-
+from .forms import * 
+from .models import *
 
 def home(request):
-    return render(request, "app/home.html")
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    
+    context = {'tasks': tasks, 'form': form}
+    
+    return render(request, "app/home.html", context)
+
+def update_task(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    
+    context = {"form": form}
+    
+    return render(request, 'app/update_task.html', context)
+
+def delete_task(request, pk):
+    task = Task.objects.get(id=pk)
+
+    if request.method == "POST":
+        task.delete()
+        return redirect("/")
+    context = {'task': task}
+    
+    return render(request, 'app/delete_task.html', context)
+    
