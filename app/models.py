6955216a51627ddaceaab84fddbab8ec2e1802ae
shiyanshirani <shiyan99s@gmail.com>
2021-09-19 14:55:37 +0530

    ModelForm initialized

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Task(models.Model):
+    task = models.CharField(max_length=256, null=False)
+    person = models.CharField(max_length=126, null=False)
+
+    def __str__(self):
+        return f"{self.task}, {self.person}"
--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .forms import TaskForm
 
-# Create your views here.
+
+
+def home(request):
+    context = {}
+    
+    form = TaskForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+    context['form'] = TaskForm()
+    return render(request, 'app/home.html', context)
